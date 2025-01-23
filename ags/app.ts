b533e830a073e794f070653f6fc9e60c3b4a5952
retrozinndev <joaovodias@gmail.com>
2025-01-23 14:05:24 -0300

    :sparkles: ags(scripts): add experimental script to reload ags on code change

--- ags/app.ts
+++ ags/app.ts
@@ -1,12 +1,13 @@
-import { Gio, monitorFile, readFile, Process } from "astal";
 import { App } from "astal/gtk3"
 
 import { Bar } from "./window/Bar";
 import { runStyleHandler } from "./scripts/style-handler";
+import "./scripts/reload-handler";
 
 runStyleHandler();
 
 App.start({
+    instanceName: "astal",
     main() {
         Bar(0);
     }
---
+++ ags/scripts/reload-handler.ts
@@ -0,0 +1,17 @@
+import { Gio, monitorFile, Process } from "astal";
+
+const monitoringPaths = [ "./scripts", "./widget", "./app.ts", "env.d.ts" ];
+
+function restartInstance(instanceName?: string) {
+    Process.exec_async(`ags run`, () => {});
+    Process.exec_async(`astal -q ${ instanceName ? instanceName : "astal" }`, () => {});
+}
+
+monitoringPaths.map((path: string) => {
+    monitorFile(
+        path,
+        () => {
+            restartInstance("astal");
+        }
+    )
+})
