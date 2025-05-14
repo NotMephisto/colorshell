7b6ada2e2b88b10fc718f5651da3c433fd8cfafb
retrozinndev <joaovodias@gmail.com>
2025-05-13 22:25:55 -0300

    :sparkles: scripts(install): add a way to skip unnecessary stuff

--- install.sh
+++ install.sh
@@ -42,12 +42,12 @@ sleep .5
 echo "Welcome to the colorshell installation script!"
 
 # Warn user of possible problems that can happen
-echo "!!!WARNING!!! By running this script, you assume total responsability for any issues that may occur"
+echo "!!!WARNING!!! By running this script, you assume total responsability for any issues that may occur with your filesystem"
 
 echo -n "Do you want to start the shell installation? [y/n] "
-read input
+[[ ! $1 == "dots" ]] && read input
 
-if [[ $input =~ "y" ]]; then
+if [[ $1 == "dots" ]] || [[ $input =~ "y" ]]; then
 	Send_log "Starting installation...\n"
 
 	for dir in ${config_dirs[@]}; do
@@ -61,12 +61,19 @@ if [[ $input =~ "y" ]]; then
     # Ask if user also wants to install default wallpapers
     Apply_wallpapers
 
-    echo "Ah yes! Looks like it's installed, yay :3"
-    echo -e "If you find any issue, please report it in: https://github.com/retrozinndev/Hyprland-Dots/issues"
-	echo "Thanks for using colorshell! I'm really appreciate that :D"
+    if ! [[ $1 == "dots" ]]; then
+        echo "Ah yes! Looks like it's installed, yay :3"; sleep .8
+        echo "If you find any issue, please report it in: https://github.com/retrozinndev/colorshell/issues"; sleep .5
+        echo "Thanks for using colorshell! I'm really appreciate that :D"
+        printf "\n"
 
-    printf "\n"
-else
-    printf "Ok, doing as you said! Bye bye!\n"
-	exit 0
+        exit 0
+    fi
+
+    Send_log "colorshell is installed!"
+
+    exit 0
 fi
+
+printf "Ok, doing as you said! Bye bye!\n"
+exit 0
